01c1d4c245d859d6c55a0f29434b51aefeeb010e
ebeatty1 <40609703+ebeatty1@users.noreply.github.com>
2019-05-16 20:18:13 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,30 @@
 #!/usr/bin/bash
 
 declare -a packages
-packages=(irssi mpv neofetch rsync screen texmaker vnstat youtube-dl)
 
-sudo pacman -S ${packages[*]} --noconfirm --needed --verbose 1> ./pac.log 2> ./pac.err
+# Un-comment the full array and comment the testing array in the final version
+# packages=(irssi mpv neofetch perl rsync screen texmaker vnstat youtube-dl)
+packages=(irssi)
+
+# Un-comment the full command in the final version
+# sudo pacman -S ${packages[*]} --noconfirm --needed --verbose 1> ./pac.log 2> ./pac.err
+sudo pacman -S ${packages[*]} --noconfirm --needed --verbose
+
+# If the flag -i is provided with an argument in the form of user@address of the backup
+# server (ie. evan@192.168.1.2), run rsync and setup all appropriate symlinks.
+while getopts ":i:" opt; do
+        case $opt in
+                i)
+                        rsync -avz "$OPTARG":/home/user/Pictures/ ~/Documents/rsync/Pictures >&2
+                        ln --symbolic -T ~./Documents/rsync/Pictures/ ~/Pictures
+                        ;;
+                \?)
+                        echo "Invalid option: -$OPTARG" >&2
+                        ;;
+                :)
+                        echo "Option -$OPTARG requires an argument." >&2
+                        ;;
+        esac
+done
+
+# I should probably just write this in perl
