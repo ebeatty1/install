01a40646ebac05c2635445fa4de061abf064d7ae
evan <ebeatty1@hawk.iit.edu>
2019-05-17 02:50:39 -0500

    Added some options, and basic installation istructions for AUR packages

--- install.sh
+++ install.sh
@@ -1,23 +1,44 @@
 #!/usr/bin/bash
 
+# Un-comment in the final version
+# pacman -Syu
+
 declare -a packages
 
 # Un-comment the full array and comment the testing array in the final version
-# packages=(irssi mpv neofetch perl rsync screen texmaker vnstat youtube-dl)
+# packages=(cmus irssi mpv neofetch perl rsync scite screen texmaker vnstat youtube-dl)
 packages=(irssi)
 
 # Un-comment the full command in the final version
 # sudo pacman -S ${packages[*]} --noconfirm --needed --verbose 1> ./pac.log 2> ./pac.err
 sudo pacman -S ${packages[*]} --noconfirm --needed --verbose
 
-# If the flag -i is provided with an argument in the form of user@address of the backup
-# server (i.e. evan@192.168.1.2), run rsync and setup all appropriate symlinks.
-while getopts ":i:" opt; do
+# Again, un-comment out the full command in the final version
+# This section is for packages in the arch user repo
+# cd ~/Downloads
+# git clone https://aur.archlinux.org/scite.git
+# cd ./scite
+# makepkg -si
+# cd ../
+# rm -R ./scite			# Should I do this? A binary gets placed in /usr/bin
+# cd ~
+
+# If the flag -r is provided with an argument in the form of user@address of the backup
+# server (i.e. evan@192.168.1.2), run rsync and setup all appropriate symlinks
+# Alternatively, if the flag -u is provided, run some basic updates
+while getopts ":r:u" opt; do
         case $opt in
-                i)
-                        rsync -avz "$OPTARG":/home/user/Pictures/ ~/Documents/rsync/Pictures >&2
-                        ln --symbolic -T ~./Documents/rsync/Pictures/ ~/Pictures
-                        ;;
+                r)
+			mkdir -p ~/Documents/rsync/
+			rsync -avz "$OPTARG":~/Pictures/ ~/Documents/rsync/Pictures/ >&2
+			rsync -avz "$OPTARG":~/Documents/Articles/ ~/Documents/rsync/Articles >&2
+                        ln --symbolic -T ~/Documents/rsync/Pictures/ ~/Pictures
+			ln --symbolic -T ~/Documents/rsync/Articles/ ~/Documents/Articles
+			;;
+		u)
+			echo "Updating" # placeholder
+					# this flag might not be necessary when I finish this up
+			;;
                 \?)
                         echo "Invalid option: -$OPTARG" >&2
                         ;;
